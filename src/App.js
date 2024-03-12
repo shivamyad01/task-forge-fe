bbd293fb83c7ca3d751e4baa8de87301f72ebcf2
AshwinDewaliya <ashiwndewaliya05@gmail.com>
2024-03-12 20:09:52 +0530

    added demo folder

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 // src/App.js
 
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-import MiniDrawer from './Components/MiniDrawer';
-import Dashboard from './Components/Dashboard';
-import ProfileManager from './Components/ProfileManager';
-import TaskManager from './Components/TaskManager';
-import Login from './Components/Login'; // Import the Login component
-import axios from 'axios';
+import MiniDrawer from "./Components/MiniDrawer";
+import Dashboard from "./Components/Dashboard";
+import ProfileManager from "./Components/ProfileManager";
+import TaskManager from "./Components/TaskManager";
+import Login from "./Components/Login"; // Import the Login component
+import axios from "axios";
 
 const App = () => {
   const [isLoggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
     // Check if the user is logged in by calling a backend route
-    axios.get('http://localhost:5001/checkLogin')
-      .then(response => {
+    axios
+      .get("http://localhost:5001/checkLogin")
+      .then((response) => {
         if (response.data.loggedIn) {
           setLoggedIn(true);
         }
       })
-      .catch(error => console.error('Error checking login status:', error));
+      .catch((error) => console.error("Error checking login status:", error));
   }, []);
 
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
-        <Route path="/" element={isLoggedIn ? <MiniDrawer /> : <Navigate to="/login" />} />
-        <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />} />
-        <Route path="/profile" element={isLoggedIn ? <ProfileManager /> : <Navigate to="/login" />} />
-        <Route path="/task" element={isLoggedIn ? <TaskManager /> : <Navigate to="/login" />} />
+        {/* <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} /> */}
+        <Route path="" element={<MiniDrawer />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<ProfileManager />} />
+        <Route path="/task" element={<TaskManager />} />
       </Routes>
     </Router>
   );
