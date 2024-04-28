0ab4089793df96e0bd49701dc1c1a1fe4506de57
Aryan521 <rajputaryan521@gmail.com>
2024-04-28 20:03:16 +0530

    MiniDrawer updated

--- src/Components/MiniDrawer.js
+++ src/Components/MiniDrawer.js
@@ -1,103 +1,102 @@
-import React, { useState } from "react";
-import { Link as RouterLink, Routes, Route } from "react-router-dom";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import SettingsIcon from "@mui/icons-material/Settings";
-import TaskIcon from "@mui/icons-material/Task";
-import Dashboard from "./Dashboard";
-import ProfileManager from "./ProfileManager";
-import TaskManager from "./TaskManager";
-import AccountMenu from "./AccountMenu";
-import Setting from "./Setting";
-import Help from "./Help";
-import HelpCenterIcon from "@mui/icons-material/HelpCenter";
-import logo from "../Components/assets/logo.png"
+import React, { useState } from 'react';
+import { Link as RouterLink, Routes, Route } from 'react-router-dom';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
+import TaskIcon from '@mui/icons-material/Task';
+import Dashboard from './Dashboard';
+import ProfileManager from './ProfileManager';
+import TaskManager from './TaskManager';
+import AccountMenu from './AccountMenu';
+import Settings from './Settings';
+import logo from '../Components/assets/logo.png';
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
+  color: '#fff',
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
+  transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: "hidden",
+  overflowX: 'hidden',
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up('sm')]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
+  color: '#fff',
 });
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin", "background-color"], {
+  transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  backgroundColor: "gray", // Change the background color here
+  backgroundColor: '#fff', // Background color set to white
+  color: '#000', // Text color set to black
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
   }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
+);
 
 export default function MiniDrawer() {
   const theme = useTheme();
@@ -112,52 +111,42 @@ export default function MiniDrawer() {
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
-    <CssBaseline />
-    <AppBar position="fixed" open={open}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerOpen}
-          edge="start"
-          sx={{
-            marginRight: 5,
-            ...(open && { display: "none" }),
-          }}
-        >
-          <MenuIcon />
-        </IconButton>
-        {/* Logo and title hidden when drawer is open */}
-        {!open && (
-          <>
-            <img src={logo} alt="Logo" style={{ marginRight: '10px', height: '40px' }} />
-         
-          </>
-        )}
-        <div style={{ marginLeft: "auto" }}>
-          <AccountMenu />{" "}
-          {/* AccountMenu will be on the completely right side */}
-        </div>
-      </Toolbar>
-    </AppBar>
-    <Drawer variant="permanent" open={open}>
-      <DrawerHeader>
-        {/* Logo and title shown when drawer is closed */}
-        <img src={logo} alt="Logo" style={{ marginRight: '10px', height: '40px' }} />
-        <IconButton onClick={handleDrawerClose}>
-          {theme.direction === "rtl" ? (
-            <ChevronRightIcon />
-          ) : (
-            <ChevronLeftIcon />
-          )}
-        </IconButton>
-      </DrawerHeader>
-  
+    <div class="animated-element">
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className='w-48 pr-5 '>
+          <img src={logo} alt="TaskForge"/>
+          </div>
+          <div style={{ marginLeft: 'auto' }}>
+            <AccountMenu /> {/* AccountMenu will be on the completely right side */}
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
         <Divider />
+        <div className='bg-white text-slate-400 text-2xl'>
         <List>
           <ListItem component={RouterLink} to="/dashboard">
-            <ListItemButton sx={{ "&:hover": { backgroundColor: "orange" } }}>
+            <ListItemButton>
               <ListItemIcon>
                 <DashboardIcon />
               </ListItemIcon>
@@ -165,7 +154,7 @@ export default function MiniDrawer() {
             </ListItemButton>
           </ListItem>
           <ListItem component={RouterLink} to="/profile">
-            <ListItemButton sx={{ "&:hover": { backgroundColor: "orange" } }}>
+            <ListItemButton>
               <ListItemIcon>
                 <AccountCircleIcon />
               </ListItemIcon>
@@ -173,44 +162,37 @@ export default function MiniDrawer() {
             </ListItemButton>
           </ListItem>
           <ListItem component={RouterLink} to="/task">
-            <ListItemButton sx={{ "&:hover": { backgroundColor: "orange" } }}>
+            <ListItemButton>
               <ListItemIcon>
                 <TaskIcon />
               </ListItemIcon>
               <ListItemText primary="Task" />
             </ListItemButton>
           </ListItem>
-          <ListItem component={RouterLink} to="/help">
-            <ListItemButton sx={{ "&:hover": { backgroundColor: "orange" } }}>
-              <ListItemIcon>
-                <HelpCenterIcon />
-              </ListItemIcon>
-              <ListItemText primary="Help" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem component={RouterLink} to="/setting">
-            <ListItemButton sx={{ "&:hover": { backgroundColor: "orange" } }}>
+          <div className='mt-96'>
+          <ListItem component={RouterLink} to="/settings">
+            <ListItemButton>
               <ListItemIcon>
                 <SettingsIcon />
               </ListItemIcon>
               <ListItemText primary="Settings" />
             </ListItemButton>
           </ListItem>
+          </div>
         </List>
+        </div>
         <Divider />
-        {/* Add additional items or footer items here */}
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/* Adjusted paddingTop to accommodate AppBar */}
         <DrawerHeader />
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/profile" element={<ProfileManager />} />
           <Route path="/task" element={<TaskManager />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/settings" element={<Setting />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </Box>
     </Box>
+    </div>
   );
 }
